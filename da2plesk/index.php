565332798be0edf77ae85a25a841f1a414e25998
Rudy Broersma <tozz@kijkt.tv>
2013-03-13 13:41:39 +0100

    Typo

--- da2plesk/index.php
+++ da2plesk/index.php
@@ -103,7 +103,7 @@ foreach ($backup->getAdditionalDomains(FALSE) as $extradomain) {
     };
     
     foreach ($backup->getForward($extradomain) as $forward) {
-        if (!in_array($forward['accounts'] . "@" . $extradomain, $mailaccounts)) {
+        if (!in_array($forward['account'] . "@" . $extradomain, $mailaccounts)) {
             // Mailaccount is not in array, so we create a new one.
           echo "/opt/psa/bin/mail -c " . $forward['account'] . "@$extradomain -mailbox false -forwarding true -forwarding-addresses add:" . $forward['to'] . "\n";
           echo "/opt/psa/bin/spamassassin -u " . $forward['account'] . "@$extradomain -status true -hits 5 -action del\n";
@@ -111,6 +111,7 @@ foreach ($backup->getAdditionalDomains(FALSE) as $extradomain) {
             // We add the forward to the already created account.
           $forward['to'] = preg_replace('/\s+/', '', $forward['to']); // remove all spaces
           echo "/opt/psa/bin/mail -u " . $forward['account'] . "@$extradomain -forwarding true -forwarding-addresses add:" . $forward['to'] . "\n";
+          array_push($mailaccounts, $forward['to'] . "@" . $extradomain);
         }
     }
 }
