09f18e035ff70be1c719f6857b5d50b3279bf2ea
Rudy Broersma <Rudy Broersma@WIN-G5AD0D79M1H>
2013-07-29 10:26:42 +0200

    Fix for Plesk 11.5: E-mail address is now required when creating customer.

--- da2plesk/index.php
+++ da2plesk/index.php
@@ -35,6 +35,7 @@ $tld = $tld[0];
 
 $ip = $backup->getIP();
 $username = $backup->getUsername();
+$acctemail = $backup->getEmail();
 
 /* BEGIN PRIMARY DOMAIN */
 echo "# Control Panel: http://www.$domain:8880/\n";
@@ -46,7 +47,7 @@ echo "# Gebruikersnaam: $username\n";
 echo "# Wachtwoord: $password\n";
 echo "#\n";
 
-echo "/opt/psa/bin/customer -c $username -name $username -passwd $password\n";
+echo "/opt/psa/bin/customer -c $username -email $acctemail -name $username -passwd $password\n";
 echo "/opt/psa/bin/subscription -c $domain -owner $username -service-plan \"" . SERVICE_PLAN . "\" -ip " . IPv4 . "," . IPv6 . " -login $username -passwd $password\n";
 echo "\n";
 echo "/usr/bin/find " . $backup->getPath() . "/domains/" . $domain . "/ -type f -print | xargs -I {} sed -i \"s@/home/" . $username . "/domains/" . $domain . "/public_html@/var/www/vhosts/" . $domain . "/httpdocs@g\" {}\n";
